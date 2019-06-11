e45ecbe28c092c266d61ee1c5577f17f0942ffbc
kommunistMaou <kommunistmaou@gmail.com>
2019-06-11 14:02:59 +0300

    Updates

---
+++ precache-manifest.d20b795366f8cc7b66d5f71f17557603.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "103bee59ca2cc902aa7ef92e347ce568",
+    "url": "/TicTacToeWithReact/index.html"
+  },
+  {
+    "revision": "6b3b2e10c345de69e342",
+    "url": "/TicTacToeWithReact/static/css/main.ebc244ca.chunk.css"
+  },
+  {
+    "revision": "66f5c75bd3d634cff85b",
+    "url": "/TicTacToeWithReact/static/js/2.19bc85eb.chunk.js"
+  },
+  {
+    "revision": "6b3b2e10c345de69e342",
+    "url": "/TicTacToeWithReact/static/js/main.fc8cecfc.chunk.js"
+  },
+  {
+    "revision": "55bec981ff4bfff89573",
+    "url": "/TicTacToeWithReact/static/js/runtime~main.1fd564e1.js"
+  }
+]);
